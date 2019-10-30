64ab8edb87b9cf56d6d5abf82475ac0277f24177
m-haketa <git@excelspeedup.com>
2019-10-30 09:08:38 +0900

    getValue、getValuesの返り値に型情報を付加できるように型定義を上書き

--- @types/ModifyExistingType.d.ts
+++ @types/ModifyExistingType.d.ts
@@ -4,3 +4,15 @@
 interface Array<T> {
   fill(value?: any, start?: number | undefined, end?: number | undefined): T[];
 }
+
+declare namespace GoogleAppsScript {
+  export namespace Spreadsheet {
+    export interface Range {
+      // 返り値に型を付けられるように修正
+      getValue<U = any>(): U;
+
+      // 返り値に型（配列、または、タプル型）を付けられるように修正
+      getValues<U extends any[] = any[]>(): U[];
+    }
+  }
+}
