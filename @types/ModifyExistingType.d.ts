28c018497d3b509aecf64a777891117727117f4e
m-haketa <git@excelspeedup.com>
2020-02-22 07:21:11 +0900

    fillの定義を元の定義に寄せる、console.logでnumberを受け付けるように修正

--- @types/ModifyExistingType.d.ts
+++ @types/ModifyExistingType.d.ts
@@ -2,7 +2,7 @@
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 interface Array<T> {
-  fill(value?: any, start?: number | undefined, end?: number | undefined): T[];
+  fill(value?: any, start?: number, end?: number): this;
 }
 
 declare namespace GoogleAppsScript {
@@ -13,6 +13,15 @@ declare namespace GoogleAppsScript {
 
       // 返り値に型（配列、または、タプル型）を付けられるように修正
       getValues<U extends any[] = any[]>(): U[];
+    }
+  }
+  export namespace Base {
+    // eslint-disable-next-line @typescript-eslint/class-name-casing
+    interface console {
+      error(formatOrObject: any): void;
+      info(formatOrObject: any): void;
+      log(formatOrObject: any): void;
+      warn(formatOrObject: any): void;
     }
   }
 }
