9a79d85cc66d85ec31e7c2209e63611903f3bd28
m-haketa <git@excelspeedup.com>
2020-10-11 11:08:15 +0900

    test用mainロジック

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import dayjs from 'dayjs';
+import 'dayjs/Locale/ja';
+
 global.main = function (): void {
-  Logger.log(1);
+  dayjs.locale('ja');
+
+  console.log(dayjs('2018-08-08'));
+  console.log(dayjs('2018-08-08').format());
+  console.log(dayjs().format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A')); // display
+  console.log(dayjs().set('month', 3).month()); // get & set
+  console.log(dayjs().add(1, 'year')); // manipulate
+  console.log(dayjs().isBefore(dayjs())); // query
 };
