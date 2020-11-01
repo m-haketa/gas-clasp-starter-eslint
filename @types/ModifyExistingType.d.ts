36738a4d6d1c41cfc72d59624227f392a717e5a9
m-haketa <git@excelspeedup.com>
2020-11-01 12:57:41 +0900

    型定義を修正（console.??を削除、getValue周りを整備）

--- @types/ModifyExistingType.d.ts
+++ @types/ModifyExistingType.d.ts
@@ -1,27 +1,20 @@
 //既存型の修正をする
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-interface Array<T> {
-  fill(value?: any, start?: number, end?: number): this;
-}
+type Cell = string | number | boolean | Date;
 
 declare namespace GoogleAppsScript {
   export namespace Spreadsheet {
     export interface Range {
       // 返り値に型を付けられるように修正
-      getValue<U = any>(): U;
+      getValue<Cell>(): Cell;
+      getValue<T>(): T;
 
       // 返り値に型（配列、または、タプル型）を付けられるように修正
-      getValues<U extends any[] = any[]>(): U[];
+      getValues<Cell>(): Cell[];
+      getValues<T>(): T[];
     }
   }
-  export namespace Base {
-    // eslint-disable-next-line @typescript-eslint/class-name-casing
-    interface console {
-      error(formatOrObject: any): void;
-      info(formatOrObject: any): void;
-      log(formatOrObject: any): void;
-      warn(formatOrObject: any): void;
-    }
-  }
+}
+
+interface Array<T> {
+  fill(value?: T, start?: number, end?: number): this;
 }
