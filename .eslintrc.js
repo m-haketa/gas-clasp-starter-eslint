a954361f5eb4058c56465790935f10d98232f3ad
m-haketa <git@excelspeedup.com>
2020-11-01 12:56:10 +0900

    eslintの設定を厳格化

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'googleappsscript/googleappsscript': true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'googleappsscript', 'import'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    sourceType: 'module',
+    createDefaultProgram: true,
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'import/first': 2,
+    'no-cond-assign': ['error', 'always'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
