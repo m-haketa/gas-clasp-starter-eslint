2d54ff0b95cff5e7db895d20631661f3d3890fc3
m-haketa <git@excelspeedup.com>
2020-11-01 13:35:18 +0900

    columnの型をstring literalで指定するように変更

--- src/setColumnType.ts
+++ src/setColumnType.ts
@@ -1,31 +1,62 @@
-export const setColumnType = <T extends Cell[]>(
-  ...initialValues: T
-): { values: (values: Cell[][]) => T[] } => {
-  return {
-    values: (values: Cell[][]): T[] => {
-      return values.map((row) =>
-        row.map((value, i) => {
-          if (value === '') {
-            return initialValues[i];
-          } else if (typeof initialValues[i] === 'string') {
-            return value.toString();
-          } else if (typeof initialValues[i] === 'number') {
-            return parseInt(value as any);
-          } else if (typeof initialValues[i] === 'boolean') {
-            return !!value;
-          } else if (initialValues[i] instanceof Date) {
-            return value instanceof Date ? value : new Date(value as any);
-          }
-          return initialValues[i];
-        })
-      ) as T[];
-    },
-  };
+const columnTypeToData = {
+  string: '',
+  number: 0,
+  boolean: false,
+  Date: new Date(1900, 0, 1),
 };
+
+type ColumnTypeToData = typeof columnTypeToData;
+type ColumnType = keyof ColumnTypeToData;
+
+type GetColumnType<T extends ColumnType[]> = {
+  [K in keyof T]: T[K] extends keyof ColumnTypeToData
+    ? ColumnTypeToData[T[K]]
+    : never;
+};
+
+export const setColumnType = <T extends ColumnType[]>(
+  ...ColumnTypes: T
+): { toValues: (values: Cell[][]) => GetColumnType<T>[] } => ({
+  toValues: (values: Cell[][]): GetColumnType<T>[] => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return values.map((row) => {
+      if (row.length > ColumnTypes.length) {
+        throw new Error(
+          'setColumnType: データの列数がColumnTypesの列数を超えています'
+        );
+      }
+
+      const rowCloned = [...row];
+      while (rowCloned.length < ColumnTypes.length) {
+        rowCloned.push('');
+      }
+
+      return rowCloned.map((value, i) => {
+        if (value === '') {
+          return columnTypeToData[ColumnTypes[i]];
+        } else if (ColumnTypes[i] === 'string') {
+          return String(value);
+        } else if (ColumnTypes[i] === 'number') {
+          return Number(value);
+        } else if (ColumnTypes[i] === 'boolean') {
+          return Boolean(value);
+        } else if (ColumnTypes[i] === 'Date') {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          return value instanceof Date ? value : new Date(value as any);
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }) as any;
+  },
+});
 
 const values = [
   ['abc', 100, true, new Date(2020, 1, 3)],
   ['', '', '', ''],
+  ['cde', 200, false],
 ];
 
-const data = setColumnType('', 0, false, new Date()).values(values);
+//使用例
+//const data = setColumnType('string', 'number', 'boolean', 'Date').toValues(
+//  values
+//);
